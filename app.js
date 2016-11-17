e4ecd7586d3e538fdc97cabfc683349e60efdce4
shelalaine <schan211@my.centennialcollege.ca>
2016-11-17 12:32:59 -0500

    Add some more resources

--- app.js
+++ app.js
@@ -11,13 +11,22 @@ server.listen(config.port, config.host, function(){
     console.log('Server %s listening at %s', config.serverName, server.url);
     console.log('Resources:');
     console.log('/patients');
-    console.log('/patients/:id');
+    console.log('/patients/:id_p');
     console.log('/patients/:id_p/vitals');
     console.log('/patients/:id_p/prescriptions');
     console.log('/patients/:id_p/tests');
     console.log('/patients/:id_p/notes');
     console.log('/patients/:id_p/doctors');
     console.log('/patients/:id_p/nurses');
+    console.log('/patients/:id_p/vitals/:id_r');
+    console.log('/patients/:id_p/prescriptions/:id_r');
+    console.log('/patients/:id_p/tests/:id_r');
+    console.log('/patients/:id_p/notes/:id_r');
+    console.log('/patients/:id_p/doctors/:id_r');
+    console.log('/patients/:id_p/nurses/:id_r');
+    console.log('Where:');
+    console.log(":id_p --> Patient's id");
+    console.log(":id_r --> Patient record's id");
 });
 
 // Allow the use of POST
