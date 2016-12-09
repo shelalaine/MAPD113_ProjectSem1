47f9a618b30e3780f682ac1242ab903323ae8578
shelalaine <schan211@my.centennialcollege.ca>
2016-12-09 09:00:17 -0500

    Update Create New Patient

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -15,40 +15,8 @@ function patientController() {
             return next(new restify.InvalidArgumentError('Last name must be supplied'));
         }
 
-        var patient = new Patient({
-            firstName: req.params.firstName, 
-            lastName: req.params.lastName,
-            birthday: req.params.birthday,
-            address: {
-                street: req.params.address.street, 
-                city: req.params.address.city, 
-                province: req.params.address.province,
-                zipCode: req.params.address.zipCode 
-            },
-            contact: {
-                phone: req.params.contact.phone,
-                email: req.params.contact.email, 
-                emergencyContactName: req.params.contact.emergencyContactName,
-                emergencyContactNumber: req.params.contact.emergencyContactNumber
-            },
-            imageName: req.params.imageName, 
-            gender: req.params.gender,
-            weight: req.params.weight,
-            height: req.params.height,
-            occupation: req.params.occupation,
-            bloodType: req.params.bloodType,
-            maritalStatus: req.params.maritalStatus,
-            condition: req.params.condition,
-            admissionDate: req.params.admissionDate,
-            dischargedDate: req.params.dischargedDate,
-            room: req.params.room,
-            insurance: {
-                name: req.params.insurance.name,
-                expiryDate: req.params.insurance.expiryDate
-            },
-            allergies: req.params.allergies
-        });
-
+        // Create new patient
+        var patient = new Patient(req.params);
         patient.save(function(err, patient){
             if (err) {
                 console.log(err);
